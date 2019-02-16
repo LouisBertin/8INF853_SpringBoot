6fb9b3adfbcfeaa56aee90b91f6acb6b4871653e
LouisBertin <bertin.louis7@gmail.com>
2019-02-16 14:49:39 -0500

    build docker db configuration

---
+++ db/mysql_init/schema.sql
@@ -0,0 +1,9 @@
+/**
+ * CREATE TABLES
+ */
+CREATE TABLE struts.test ( column_name1 INT, column_name2 VARCHAR(100) , column_name3 INT );
+
+/**
+ * INSERT DATA
+ */
+INSERT INTO test (column_name1, column_name2, column_name3) VALUES (1,"Tacos", 2), (2,"Tomato Soup", 3), (3,"Grilled Cheese", 4);
