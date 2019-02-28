11cabad3a0e460c6f664df3b5b4db84eb2d7b182
LouisBertin <bertin.louis7@gmail.com>
2019-02-28 18:11:48 -0500

    bootstrap works

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -4,10 +4,7 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * The type Welcome controller.
@@ -57,6 +54,17 @@ public class WelcomeController {
         model.addAttribute("user", new User());
 
         return "adduser";
+    }
+
+    /**
+     * Test to get url param
+     */
+    @RequestMapping(value="/test/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String test(@PathVariable("id") int id) {
+        System.out.println(id);
+
+        return "Test";
     }
 
     /**
