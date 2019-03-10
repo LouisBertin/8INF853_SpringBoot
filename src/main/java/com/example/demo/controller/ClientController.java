1e3f3a9e6c92bc449279a676514dfc243827bb5b
LouisBertin <bertin.louis7@gmail.com>
2019-03-09 21:54:16 -0500

    fix merge and fix details

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -50,18 +50,6 @@ public class ClientController {
         return "figurines";
     }
 
-    @GetMapping(value="/figurines/categories")
-    public String displayCategories(Model model){
-        model.addAttribute("categories_", categorieRepository.findAll());
-        return "categories";
-    }
-
-    @GetMapping(value="/figurines/marques")
-    public String displayMarques(Model model){
-        model.addAttribute("marques_", marqueRepository.findAll());
-        return "marques";
-    }
-
     @GetMapping(value="/figurines/reservation/{id}")
     public String reserverFigurine(Model model, @PathVariable("id") int id){
         Figurine figurine = figurineRepository.findById(id).get();
